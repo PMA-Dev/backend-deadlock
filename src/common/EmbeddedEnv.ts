583cd1e001a420840da3d69f941f11b079eaa9d3
astherath <farceriv@gmail.com>
2024-09-19 15:53:15 -0400

    Added env

---
+++ src/common/EmbeddedEnv.ts
@@ -0,0 +1,12 @@
+export const embeddedEnv = {
+  "AKV_LOCAL_CERT_PATH": "../../akv_test_cert.pem",
+  "LOADED_DOTENV_PATH": "../../.env",
+  "AZURE_STORAGE_QUEUE_NAME": "sms-paid-queue",
+  "AZURE_KEY_VAULT_SECRET_NAME": "localenv",
+  "AZURE_KEY_VAULT_NAME": "backend-akv",
+  "AZURE_TENANT_ID": "a8c774b1-1aef-4d11-b518-60c92deea92d",
+  "AZURE_CLIENT_ID": "044b938d-a250-48ed-9477-cc8cdfd7e85f",
+  "SECRET_FILE_VER": "9764fb0784c24b2594b7eb87e0762f7a",
+  "PORT_TO_USE": "5080",
+  "HOST_TO_USE": "127.0.0.1"
+};
